4b19b8ca99ae6aebd6718652bb7f6784c389950d
clover-zq <60808831+clover-zq@users.noreply.github.com>
2020-05-04 21:49:10 +0800

    没加东西

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -1,8 +1,6 @@
 import Axios from 'axios'
 import defu from 'defu'
 
-const globalName = '$nuxt'
-
 // Axios.prototype cannot be modified
 const axiosExtra = {
   setBaseURL (baseURL) {
@@ -82,7 +80,7 @@ const setupProgress = (axios) => {
     set: () => { }
   }
 
-  const $loading = () => (window[globalName] && window[globalName].$loading && window[globalName].$loading.set) ? window[globalName].$loading : noopLoading
+  const $loading = () => (window.$nuxt && window.$nuxt.$loading && window.$nuxt.$loading.set) ? window.$nuxt.$loading : noopLoading
 
   let currentRequests = 0
 
@@ -160,13 +158,14 @@ export default (ctx, inject) => {
   }
 
   // Proxy SSR request headers headers
-  if (process.server && ctx.req && ctx.req.headers) {
-    const reqHeaders = { ...ctx.req.headers }
-    for (let h of ["accept","host","cf-ray","cf-connecting-ip","content-length","content-md5","content-type"]) {
-      delete reqHeaders[h]
-    }
-    axiosOptions.headers.common = { ...reqHeaders, ...axiosOptions.headers.common }
-  }
+  axiosOptions.headers.common = (ctx.req && ctx.req.headers) ? Object.assign({}, ctx.req.headers) : {}
+  delete axiosOptions.headers.common['accept']
+  delete axiosOptions.headers.common['host']
+  delete axiosOptions.headers.common['cf-ray']
+  delete axiosOptions.headers.common['cf-connecting-ip']
+  delete axiosOptions.headers.common['content-length']
+  delete axiosOptions.headers.common['content-md5']
+  delete axiosOptions.headers.common['content-type']
 
   if (process.server) {
     // Don't accept brotli encoding because Node can't parse it
--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,18 +3,18 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _6c2e7a06 = () => interopDefault(import('..\\pages\\air\\index.vue' /* webpackChunkName: "pages_air_index" */))
-const _34aafdda = () => interopDefault(import('..\\pages\\hotel\\index.vue' /* webpackChunkName: "pages_hotel_index" */))
-const _0eece8f7 = () => interopDefault(import('..\\pages\\map.vue' /* webpackChunkName: "pages_map" */))
-const _19c2b64d = () => interopDefault(import('..\\pages\\post\\index.vue' /* webpackChunkName: "pages_post_index" */))
-const _2960caee = () => interopDefault(import('..\\pages\\air\\flights.vue' /* webpackChunkName: "pages_air_flights" */))
-const _3a98f90e = () => interopDefault(import('..\\pages\\air\\order.vue' /* webpackChunkName: "pages_air_order" */))
-const _30a07893 = () => interopDefault(import('..\\pages\\air\\pay.vue' /* webpackChunkName: "pages_air_pay" */))
-const _67f8b0e0 = () => interopDefault(import('..\\pages\\hotel\\detail.vue' /* webpackChunkName: "pages_hotel_detail" */))
-const _396e53e6 = () => interopDefault(import('..\\pages\\post\\dateil.vue' /* webpackChunkName: "pages_post_dateil" */))
-const _eea1dfa2 = () => interopDefault(import('..\\pages\\post\\post-release.vue' /* webpackChunkName: "pages_post_post-release" */))
-const _119ced59 = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages_user_login" */))
-const _407fe6a6 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _6a6eea1d = () => interopDefault(import('..\\pages\\air\\index.vue' /* webpackChunkName: "pages_air_index" */))
+const _7d436033 = () => interopDefault(import('..\\pages\\hotel\\index.vue' /* webpackChunkName: "pages_hotel_index" */))
+const _4eb2f452 = () => interopDefault(import('..\\pages\\map.vue' /* webpackChunkName: "pages_map" */))
+const _ebfd19a6 = () => interopDefault(import('..\\pages\\post\\index.vue' /* webpackChunkName: "pages_post_index" */))
+const _7e0cabe4 = () => interopDefault(import('..\\pages\\air\\flights.vue' /* webpackChunkName: "pages_air_flights" */))
+const _f98caace = () => interopDefault(import('..\\pages\\air\\order.vue' /* webpackChunkName: "pages_air_order" */))
+const _44c5e7b3 = () => interopDefault(import('..\\pages\\air\\pay.vue' /* webpackChunkName: "pages_air_pay" */))
+const _79089480 = () => interopDefault(import('..\\pages\\hotel\\detail.vue' /* webpackChunkName: "pages_hotel_detail" */))
+const _5df199f4 = () => interopDefault(import('..\\pages\\post\\dateil.vue' /* webpackChunkName: "pages_post_dateil" */))
+const _38ac174f = () => interopDefault(import('..\\pages\\post\\post-release.vue' /* webpackChunkName: "pages_post_post-release" */))
+const _fc48ab8e = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages_user_login" */))
+const _2185f18d = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -34,51 +34,51 @@ export const routerOptions = {
 
   routes: [{
     path: "/air",
-    component: _6c2e7a06,
+    component: _6a6eea1d,
     name: "air"
   }, {
     path: "/hotel",
-    component: _34aafdda,
+    component: _7d436033,
     name: "hotel"
   }, {
     path: "/map",
-    component: _0eece8f7,
+    component: _4eb2f452,
     name: "map"
   }, {
     path: "/post",
-    component: _19c2b64d,
+    component: _ebfd19a6,
     name: "post"
   }, {
     path: "/air/flights",
-    component: _2960caee,
+    component: _7e0cabe4,
     name: "air-flights"
   }, {
     path: "/air/order",
-    component: _3a98f90e,
+    component: _f98caace,
     name: "air-order"
   }, {
     path: "/air/pay",
-    component: _30a07893,
+    component: _44c5e7b3,
     name: "air-pay"
   }, {
     path: "/hotel/detail",
-    component: _67f8b0e0,
+    component: _79089480,
     name: "hotel-detail"
   }, {
     path: "/post/dateil",
-    component: _396e53e6,
+    component: _5df199f4,
     name: "post-dateil"
   }, {
     path: "/post/post-release",
-    component: _eea1dfa2,
+    component: _38ac174f,
     name: "post-post-release"
   }, {
     path: "/user/login",
-    component: _119ced59,
+    component: _fc48ab8e,
     name: "user-login"
   }, {
     path: "/",
-    component: _407fe6a6,
+    component: _2185f18d,
     name: "index"
   }],
 
