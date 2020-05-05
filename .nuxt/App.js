167880aab0d4c1a57a7366281bbd29304591b3e2
clover-zq <60808831+clover-zq@users.noreply.github.com>
2020-05-05 09:00:57 +0800

    更新依赖

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -11,17 +11,17 @@ import {
 import NuxtLoading from './components/nuxt-loading.vue'
 import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
-import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import '..\\node_modules\\element-ui\\lib\\theme-chalk\\index.css'
 
-import '../assets/main.css'
+import '..\\assets\\main.css'
 
-import '../node_modules/quill/dist/quill.core.css'
+import '..\\node_modules\\quill\\dist\\quill.core.css'
 
-import '../node_modules/quill/dist/quill.snow.css'
+import '..\\node_modules\\quill\\dist\\quill.snow.css'
 
-import '../node_modules/quill/dist/quill.bubble.css'
+import '..\\node_modules\\quill\\dist\\quill.bubble.css'
 
-import _6f6c098b from '../layouts/default.vue'
+import _6f6c098b from '..\\layouts\\default.vue'
 
 const layouts = { "_default": sanitizeComponent(_6f6c098b) }
 
